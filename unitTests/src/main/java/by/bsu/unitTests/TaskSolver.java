6761bb5665b1424d24167a608b2c7f825836a5be
VitalyMikulich <mr.wital1999@gmail.com>
2019-11-01 18:24:45 +0300

    rewrite test in unitTesting

--- unitTests/src/main/java/by/bsu/unitTests/TaskSolver.java
+++ unitTests/src/main/java/by/bsu/unitTests/TaskSolver.java
@@ -1,8 +1,20 @@
 package by.bsu.unitTests;
 
+import by.bsu.unitTests.exception.InvalidValueOfSideException;
+
 public class TaskSolver {
-    public static boolean isTriangle(int[] arr) {
-        return arr[0] + arr[1] > arr[2] && arr[0] + arr[2] > arr[1] && arr[1] + arr[2] > arr[0] ? true : false;
+
+    public static boolean isTriangle(double a, double b, double c) throws InvalidValueOfSideException {
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new InvalidValueOfSideException("One or more sides are equal or less then zero");
+        }
+
+        if (Double.isNaN(a) || Double.isNaN(b) || Double.isNaN(c)) {
+            throw new InvalidValueOfSideException("One or more sides are equal to NaN");
+        }
+
+        return a + b > c && a + c > b && b + c > a ? true : false;
     }
 }
 
--- unitTests/src/test/java/by/bsu/unitTests/TaskSolverTest.java
+++ unitTests/src/test/java/by/bsu/unitTests/TaskSolverTest.java
@@ -1,53 +1,116 @@
 package by.bsu.unitTests;
 
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import org.junit.jupiter.api.BeforeEach;
+import by.bsu.unitTests.exception.InvalidValueOfSideException;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.runners.Parameterized;
 
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
+//@RunWith(DataProviderRunner.class)
 public class TaskSolverTest {
-    int[] t1;
-    int[] t2;
-    int[] t3;
-    int[] t4;
-    int[] t5;
-    int[] t6;
-    int[] t7;
-    int[] t8;
-    int[] t9;
-    int[] t10;
-    int[][] allTrueTr;
-    int[][] allFalseTr;
 
-    @BeforeEach
-    public void initialization() {
-        t1 = new int[]{5, 5, 5}; //true
-        t2 = new int[]{5, 5, 7}; //true
-        t3 = new int[]{5, 3, 4}; //true
-        t4 = new int[]{3, 1, 1}; //false
-        t5 = new int[]{5, 10, 7}; //true
-        t6 = new int[]{2, 5, 7}; //false
-        t7 = new int[]{20, 100, 30}; //false
-        t8 = new int[]{10, 10, 10}; //true
-        t9 = new int[]{5, 2, 1}; //false
-        t10 = new int[]{5, 5, 10}; //false
-        allTrueTr = new int[][]{t1, t2, t3, t5, t8};
-        allFalseTr = new int[][]{t4, t6, t7, t9, t10};
+//    @BeforeEach
+//    public void initialization() {
+//        t1 = new int[]{5, 5, 5}; //true
+//        t2 = new int[]{5, 5, 7}; //true
+//        t3 = new int[]{5, 3, 4}; //true
+//        t4 = new int[]{3, 1, 1}; //false
+//        t5 = new int[]{5, 10, 7}; //true
+//        t6 = new int[]{2, 5, 7}; //false
+//        t7 = new int[]{20, 100, 30}; //false
+//        t8 = new int[]{10, 10, 10}; //true
+//        t9 = new int[]{5, 2, 1}; //false
+//        t10 = new int[]{5, 5, 10}; //false
+//        allTrueTr = new int[][]{t1, t2, t3, t5, t8};
+//        allFalseTr = new int[][]{t4, t6, t7, t9, t10};
+//    }
+
+
+
+    @Test
+    public void rightTriangleTest() throws InvalidValueOfSideException {
+      assertTrue(TaskSolver.isTriangle(5, 4, 3));
     }
 
     @Test
-    public void isTriangleTrue() {
-        for(int i = 0; i < allTrueTr.length; i++) {
-            assertTrue(TaskSolver.isTriangle(allTrueTr[i]));
-        }
+    public void equilateralTriangleTest() throws InvalidValueOfSideException {
+        assertTrue(TaskSolver.isTriangle(5, 5, 5));
     }
+
     @Test
-    public void isTriangleFalse() {
-        for(int i = 0; i < allTrueTr.length; i++) {
-            assertFalse(TaskSolver.isTriangle(allFalseTr[i]));
-        }
+    public void isoscelesTriangleTest() throws InvalidValueOfSideException {
+        assertTrue(TaskSolver.isTriangle(5, 5, 7));
     }
+
+    @Test
+    public void arbitraryTriangleTest() throws InvalidValueOfSideException {
+        assertTrue(TaskSolver.isTriangle(5, 10, 7));
+    }
+
+    @Test
+    public void sideBiggerThanSumOfTwoAnotherTest() throws InvalidValueOfSideException {
+        assertFalse(TaskSolver.isTriangle(3, 1, 1));
+    }
+
+    @Test
+    public void sideEqualToTheSumOfTwoAnotherTest() throws InvalidValueOfSideException {
+        assertFalse(TaskSolver.isTriangle(5, 5, 10));
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void negativeSideTriangleTest() {
+        InvalidValueOfSideException exception =
+                                        assertThrows(InvalidValueOfSideException.class,
+                                                () -> TaskSolver.isTriangle(5, 4 ,-5));
+        assertEquals("One or more sides are equal or less then zero", exception.getMessage());
+    }
+
+    @Test
+    public void zeroSideTriangleTest() {
+        InvalidValueOfSideException exception =
+                assertThrows(InvalidValueOfSideException.class,
+                        () -> TaskSolver.isTriangle(5, 4 ,0));
+        assertEquals("One or more sides are equal or less then zero", exception.getMessage());
+    }
+
+    @Test
+    public void nanSideTriangleTest() {
+        InvalidValueOfSideException exception =
+                assertThrows(InvalidValueOfSideException.class,
+                        () -> TaskSolver.isTriangle(Double.NaN, 4 ,5));
+        assertEquals("One or more sides are equal to NaN", exception.getMessage());
+    }
+
+
+
+
+
+//    @DataProvider
+//    public static Object[] data() {
+//        return new Object[][] {
+//                { new int[]{5, 5, 5}, true },
+//                { new int[]{5, 5, 5}, true },
+//        };
+//    }
+//
+//    @Test
+//    @UseDataProvider("data")
+//    public void testIsTriangle(final int[] input, final boolean expected) {
+//        assertEquals(expected, TaskSolver.isTriangle(input));
+//    }
 }
