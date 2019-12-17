ad942714232ac440d4142cc0968fba442239e82a
VitalyMikulich <mr.wital1999@gmail.com>
2019-12-17 17:32:09 +0300

    delete some commented code

--- Lab-3/webdriver/src/test/java/by/bsu/expedia/page/AbstractPage.java
+++ Lab-3/webdriver/src/test/java/by/bsu/expedia/page/AbstractPage.java
@@ -36,17 +36,4 @@ public abstract class AbstractPage {
                 .until(expectedCondition);
         LOGGER.info("Finish waiting");
     }
-
-
-
-
-//    protected final int WAIT_TIMEOUT_SECONDS = 40;
-//
-//    protected WebDriver webDriver;
-//    protected abstract AbstractPage openPage();
-//
-//
-//    protected AbstractPage(WebDriver webDriver) {
-//        this.webDriver = webDriver;
-//    }
 }
