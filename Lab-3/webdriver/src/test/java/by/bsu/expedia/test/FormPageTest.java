7bbed2a0e3e125e8ef748809c1eb3c9f3a83ad6d
VitalyMikulich <mr.wital1999@gmail.com>
2019-12-24 15:36:53 +0300

    update

--- Lab-3/webdriver/src/test/java/by/bsu/expedia/test/FormPageTest.java
+++ Lab-3/webdriver/src/test/java/by/bsu/expedia/test/FormPageTest.java
@@ -14,70 +14,66 @@ import java.util.concurrent.TimeUnit;
 public class FormPageTest extends CommonConditions {
     @Test(testName = "testcase 1: Search car with empty pick-up field")
     public void searchCarsForEmptyPickUpPlaceTest() {
-//        FormPage page = new FormPage(driver);
-//        page.openPage().fillFromParams(CarReservationCreator.withEmptyPickUpField()).search();
-//        PageError expectedError = PageErrorCreator.errorForEmptyPickUpFieldFromProperty();
-//        Assert.assertTrue(page.checkEmptyPickUpPlaceFieldErrorMessage(expectedError));
+        FormPage page = new FormPage(driver);
+        page.openPage().fillFromParams(CarReservationCreator.withEmptyPickUpField()).search();
+        PageError expectedError = PageErrorCreator.errorForEmptyPickUpFieldFromProperty();
+        Assert.assertTrue(page.checkEmptyPickUpPlaceFieldErrorMessage(expectedError));
         Assert.assertTrue(true);
     }
 
     @Test(testName = "testcase 2: Search car with empty fields")
     public void searchCarsWithEmptyFieldsTest() {
-//        FormPage page = new FormPage(driver);
-//        page.openPage().fillFromParams(CarReservationCreator.withEmptyFields()).search();
-//        PageError expectedErrorForPickUpPlace = PageErrorCreator.errorForEmptyPickUpFieldFromProperty();
-        Assert.assertTrue(true);
+        FormPage page = new FormPage(driver);
+        page.openPage().fillFromParams(CarReservationCreator.withEmptyFields()).search();
+        PageError expectedErrorForPickUpPlace = PageErrorCreator.errorForEmptyPickUpFieldFromProperty();
+        Assert.assertTrue(page.checkEmptyPickUpPlaceFieldErrorMessage(expectedErrorForPickUpPlace));
 
-//        PageError expectedErrorForPickUpDate = PageErrorCreator.errorForEmptyPickUpDateFieldFromProperty();
-        Assert.assertTrue(true);
+        PageError expectedErrorForPickUpDate = PageErrorCreator.errorForEmptyPickUpDateFieldFromProperty();
+        Assert.assertTrue(page.checkEmptyPickUpDateFieldErrorMessage(expectedErrorForPickUpDate));
 
-//        PageError expectedErrorForDropOffDate = PageErrorCreator.errorForEmptyDropOffDateFieldFromProperty();
-        Assert.assertTrue(true);
+        PageError expectedErrorForDropOffDate = PageErrorCreator.errorForEmptyDropOffDateFieldFromProperty();
+        Assert.assertTrue(page.checkEmptyDropOffDateFieldErrorMessage(expectedErrorForDropOffDate));
     }
 
     @Test(testName = "testcase 3: Search car at not available place")
     public void searchCarsAtNotAvailablePlaceTest() {
-//        FormPage page = new FormPage(driver);
-//        page.openPage().fillFromParams(CarReservationCreator.withNotAvailablePlaceFromProperty()).search();
-//        PageError expectedError = PageErrorCreator.errorForNotAvailablePlaceFromProperty();
-//        Assert.assertTrue(page.checkInexistentPickUpPlaceErrorMessage(expectedError));
-        Assert.assertTrue(true);
+        FormPage page = new FormPage(driver);
+        page.openPage().fillFromParams(CarReservationCreator.withNotAvailablePlaceFromProperty()).search();
+        PageError expectedError = PageErrorCreator.errorForNotAvailablePlaceFromProperty();
+        Assert.assertTrue(page.checkInexistentPickUpPlaceErrorMessage(expectedError));
     }
 
     @Test(testName = "testcase 4: Search car for past pick-up date")
     public void searchCarsForPastPickUpDateTest() {
-//        FormPage page = new FormPage(driver);
-//        page.openPage().fillFromParams(CarReservationCreator.withPastPickUpDateFromProperty()).search();
-//        PageError expectedError = PageErrorCreator.errorForPastPickUpDateFromProperty();
-//        Assert.assertTrue(page.checkPickUpTimeErrorMessage(expectedError));
-        Assert.assertTrue(true);
+        FormPage page = new FormPage(driver);
+        page.openPage().fillFromParams(CarReservationCreator.withPastPickUpDateFromProperty()).search();
+        PageError expectedError = PageErrorCreator.errorForPastPickUpDateFromProperty();
+        Assert.assertTrue(page.checkPickUpTimeErrorMessage(expectedError));
     }
 
     @Test(testName = "testcase 5: Search car for equal pick-up date and drop-off date")
     public void searchCarsForEqualPickUpDateAndDropOffDateTest() {
-//        FormPage page = new FormPage(driver);
-//        page.openPage().fillFromParams(CarReservationCreator.withEqualPickUpDateAndDropOffDateFromProperty()).search();
-//        PageError expectedError = PageErrorCreator.errorForEqualityOfPickupAndDropOffTimeErrorMessage();
-//        Assert.assertTrue(page.checkEqualityOfPickupAndDropOffTimeErrorMessage(expectedError));
-        Assert.assertTrue(true);
+        FormPage page = new FormPage(driver);
+        page.openPage().fillFromParams(CarReservationCreator.withEqualPickUpDateAndDropOffDateFromProperty()).search();
+        PageError expectedError = PageErrorCreator.errorForEqualityOfPickupAndDropOffTimeErrorMessage();
+        Assert.assertTrue(page.checkEqualityOfPickupAndDropOffTimeErrorMessage(expectedError));
     }
 
     @Test(testName = "testcase 6: Search car for pick-up and return places across continents")
     public void searchCarsForPickUpAndReturnPlacesAcrossContinentsTest() {
-//        FormPage page = new FormPage(driver);
-//        page.openPage().fillFromParams(CarReservationCreator.withPickUpAndReturnPlacesAcrossContinentsFromProperty()).search();
-//        PageError expectedError = PageErrorCreator.errorForPickUpAndReturnPlacesAcrossContinentsFromProperty();
-//        Assert.assertTrue(page.checkPickUpAndReturnPlacesAcrossContinentsErrorMessage(expectedError));
-        Assert.assertTrue(true);
+        FormPage page = new FormPage(driver);
+        page.openPage().fillFromParams(CarReservationCreator.withPickUpAndReturnPlacesAcrossContinentsFromProperty()).search();
+        PageError expectedError = PageErrorCreator.errorForPickUpAndReturnPlacesAcrossContinentsFromProperty();
+        Assert.assertTrue(page.checkPickUpAndReturnPlacesAcrossContinentsErrorMessage(expectedError));
     }
 
     @Test(testName = "testcase 7: Login with not registered email")
     public void loginWithNotRegisteredEmailTest() {
-//        Account testAccount = AccountCreator.withNotRegisteredEmailOrPassword();
-//        FormPage page = new FormPage(driver);
-//        page.login(testAccount);
-//        PageError expectedError = PageErrorCreator.errorForNotRegisteredEmailOrPasswordFromProperty();
-//        Assert.assertTrue(page.checkloginErrorMessage(expectedError));
+        Account testAccount = AccountCreator.withNotRegisteredEmailOrPassword();
+        FormPage page = new FormPage(driver);
+        page.login(testAccount);
+        PageError expectedError = PageErrorCreator.errorForNotRegisteredEmailOrPasswordFromProperty();
+        Assert.assertTrue(page.checkloginErrorMessage(expectedError));
         Assert.assertTrue(true);
     }
 
